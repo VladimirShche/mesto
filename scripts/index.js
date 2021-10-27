37f9f18d941a7f105cd2f0c8e7999902e7a878fd
VladimirShche <dark.second@yandex.ru>
2021-10-27 12:17:01 +0300

    feat: возможность изменять профиль

--- scripts/index.js
+++ scripts/index.js
@@ -23,17 +23,37 @@ popupOpenButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
 popupElement.addEventListener('click', closePopupByClickOverlay);
 
+// Находим форму в DOM
+let formElement = document.querySelector('.popup__form');
+// Находим поля формы в DOM
 let nameInput = document.querySelector('.popup__name');
-let nameProf = document.querySelector('.profile__name');
 let jobInput = document.querySelector('.popup__description');
+let nameProf = document.querySelector('.profile__name');
 let jobProf = document.querySelector('.profile__subtitle');
-
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
 function formSubmitHandler (evt) {
-    evt.preventDefault();
+    evt.preventDefault(); 
     nameProf.textContent = nameInput.value;
-    jobProf.textContent = jobProf.value;
-};
+    jobProf.textContent = jobInput.value;
 
-let formElement = document.querySelector('.popup__submit');
+}
 
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', formSubmitHandler);
+
+// let nameInput = document.querySelector('.popup__name');
+// let nameProf = document.querySelector('.profile__name');
+// let jobInput = document.querySelector('.popup__description');
+// let jobProf = document.querySelector('.profile__subtitle');
+
+// function formSubmitHandler(evt) {
+//     evt.preventDefault();
+//     nameProf.textContent = nameInput.value;
+//     jobProf.textContent = jobInput.value;
+// };
+
+// let formElement = document.querySelector('.popup__submit');
+
+// formElement.addEventListener('submit', formSubmitHandler);
