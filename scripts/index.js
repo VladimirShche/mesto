f28ae2e3aa88cdeb0b5125ff213eb9027275710d
VladimirShche <dark.second@yandex.ru>
2021-11-02 00:00:04 +0300

    fix: Замечания ревьювера

--- scripts/index.js
+++ scripts/index.js
@@ -1,15 +1,13 @@
+// Находим форму в DOM
+let formElement = document.querySelector('.popup__form');
+// Находим поля формы в DOM
+let nameInput = document.querySelector('.popup__name');
+let jobInput = document.querySelector('.popup__description');
+let nameProf = document.querySelector('.profile__name');
+let jobProf = document.querySelector('.profile__subtitle');
 const popupElement = document.querySelector('.popup');
 const popupCloseButtonElement = popupElement.querySelector('.popup__close');
 const popupOpenButtonElement = document.querySelector('.profile__edit');
-
-const closePopupByClickOverlay = function(event) {
-    console.log(event.target, event.currentTarget);
-    if (event.target !== event.currentTarget) {
-        return;
-    }
-
-    closePopup();
-};
 
 const openPopup = function() {
     popupElement.classList.add('popup__is-opened');
@@ -21,15 +19,7 @@ const closePopup = function() {
 
 popupOpenButtonElement.addEventListener('click', openPopup);
 popupCloseButtonElement.addEventListener('click', closePopup);
-popupElement.addEventListener('click', closePopupByClickOverlay);
 
-// Находим форму в DOM
-let formElement = document.querySelector('.popup__form');
-// Находим поля формы в DOM
-let nameInput = document.querySelector('.popup__name');
-let jobInput = document.querySelector('.popup__description');
-let nameProf = document.querySelector('.profile__name');
-let jobProf = document.querySelector('.profile__subtitle');
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
 function formSubmitHandler (evt) {
