36edc172174c9bf500e32d1ded446229d466463e
VladimirShche <dark.second@yandex.ru>
2021-11-09 11:19:00 +0300

    final

--- scripts/index.js
+++ scripts/index.js
@@ -8,20 +8,20 @@ let jobProf = document.querySelector('.profile__subtitle');
 let popupElement = document.querySelector('.popup');
 let popupCloseButtonElement = popupElement.querySelector('.popup__close');
 let popupOpenButtonElement = document.querySelector('.profile__edit');
-let openPopup = function() {
+let openPopup = function () {
     popupElement.classList.add('popup_is-opened');
     nameInput.value = nameProf.textContent;
     jobInput.value = jobProf.textContent;
 };
-let closePopup = function() {
+let closePopup = function () {
     popupElement.classList.remove('popup_is-opened');
 };
 
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
-function formSubmitHandler (evt) {
-    evt.preventDefault(); 
+function formSubmitHandler(evt) {
+    evt.preventDefault();
     nameProf.textContent = nameInput.value;
     jobProf.textContent = jobInput.value;
     closePopup();
